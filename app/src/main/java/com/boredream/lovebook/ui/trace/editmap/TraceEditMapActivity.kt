a92f3faa3ad5b55644c3c726529edd87197721f4
boredream <48262906@qq.com>
2022-11-29 13:34:05 +0800

    edit map

---
+++ app/src/main/java/com/boredream/lovebook/ui/trace/editmap/TraceEditMapActivity.kt
@@ -0,0 +1,79 @@
+package com.boredream.lovebook.ui.trace.editmap
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.blankj.utilcode.util.FileIOUtils
+import com.boredream.lovebook.R
+import com.boredream.lovebook.base.BaseActivity
+import com.boredream.lovebook.data.TraceRecord
+import com.boredream.lovebook.data.constant.BundleKey
+import com.boredream.lovebook.data.repo.TraceRecordRepository
+import com.boredream.lovebook.databinding.ActivityTraceEditMapBinding
+import com.boredream.lovebook.utils.TraceFilter
+import com.google.gson.Gson
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class TraceEditMapActivity : BaseActivity<TraceEditMapViewModel, ActivityTraceEditMapBinding>() {
+
+    private lateinit var data: TraceRecord
+
+    override fun getLayoutId() = R.layout.activity_trace_edit_map
+
+    override fun getViewModelClass() = TraceEditMapViewModel::class.java
+
+    companion object {
+
+        fun start(context: Context, data: TraceRecord) {
+            val intent = Intent(context, TraceEditMapActivity::class.java)
+            intent.putExtra(BundleKey.DATA, data)
+            context.startActivity(intent)
+        }
+    }
+
+    @Inject
+    lateinit var repo : TraceRecordRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        // data = intent.extras?.getSerializable(BundleKey.DATA) as TraceRecord
+        // FIXME: 4 test
+        val json = FileIOUtils.readFile2String(
+            "/data/data/com.boredream.lovebook/files/trace/轨迹 2022-11-27 15:10:09.txt")
+        val oldData = Gson().fromJson(json, TraceRecord::class.java)
+        val filter = TraceFilter()
+        data = TraceRecord(
+            ArrayList(oldData.traceList.filter { filter.filterPos(it) }),
+            "test", 0, 0, 100f
+        )
+
+        viewModel.start(data)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+}
---
+++ app/src/main/java/com/boredream/lovebook/ui/trace/editmap/TraceEditMapViewModel.kt
@@ -0,0 +1,50 @@
+package com.boredream.lovebook.ui.trace.editmap
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.boredream.lovebook.base.BaseRequestViewModel
+import com.boredream.lovebook.data.TraceLocation
+import com.boredream.lovebook.data.TraceRecord
+import com.boredream.lovebook.data.repo.TraceRecordRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TraceEditMapViewModel @Inject constructor(
+    private val repository: TraceRecordRepository
+) : BaseRequestViewModel<TraceRecord>() {
+
+    private var step = 1800
+    private lateinit var data: TraceRecord
+    private var traceList = ArrayList<TraceLocation>()
+
+    private val _traceListUiState = MutableLiveData(ArrayList<ArrayList<TraceLocation>>())
+    val traceListUiState: LiveData<ArrayList<ArrayList<TraceLocation>>> = _traceListUiState
+
+    private val _startLocationUiState = MutableLiveData<TraceLocation>()
+    val startLocationUiState: LiveData<TraceLocation> = _startLocationUiState
+
+    /**
+     * 页面开始时，绘制路线，并跳转到对应位置
+     */
+    fun start(data: TraceRecord) {
+        this.data = data
+
+        // TODO:  根据路线，选择合适的 camera zoom 和 position
+        _startLocationUiState.value = data.traceList[step]
+    }
+
+    fun preStepTrace() {
+        traceList.removeLast()
+        _traceListUiState.value = arrayListOf(traceList)
+        step -= 1
+    }
+
+    fun nextStepTrace() {
+        val newTracePoint = data.traceList[step]
+        traceList.add(newTracePoint)
+        _traceListUiState.value = arrayListOf(traceList)
+        step += 1
+    }
+
+}
